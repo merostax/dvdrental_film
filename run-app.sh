98760c17973c83dc8af41e3548fab1dcc2c55ad4
kt <kt@ciphron.de>
2023-11-14 12:33:38 +0100

    add run-app.sh

--- run-app.sh
+++ run-app.sh
@@ -6,5 +6,7 @@ export POSTGRESQL_SERVICE_PORT=54321
 export POSTGRESQL_USER=postgres
 export POSTGRESQL_PASSWORD=postgres
 export POSTGRESQL_DATASOURCE=FilmDBDS
+mvn clean package
+java -jar target/film-bootable.jar -Djboss.http.port=8081
 
-mvn clean package && mvn wildfly-jar:run
+
